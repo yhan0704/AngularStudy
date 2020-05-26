8e00f44d45cd4324e721c9cc64f4666c9b097fd2
github username <hanyc1205@gmail.com>
2020-05-26 15:28:45 -0400

    http post

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -9,12 +9,24 @@ import { HttpClient } from  '@angular/common/http'
 export class PostComponent implements OnInit {
 
   posts: any = []
+  url: string = "https://jsonplaceholder.typicode.com/posts"
 
-  constructor(http : HttpClient) { 
-    http.get("https://jsonplaceholder.typicode.com/posts")
+  constructor(private http : HttpClient) { 
+    http.get(this.url)
     .subscribe(res => {
       this.posts  = res;
     })
+  }
+
+  createPost(input){
+    let title = {title: input.value}
+    input.value= ""
+    this.http.post(this.url, JSON.stringify(title))
+    .subscribe((res:{id}) => {
+      title['id'] = res.id
+      this.posts.splice(0,0, title);
+    })
+    
   }
 
   ngOnInit(): void {
