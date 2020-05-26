4b056625013e509abdda9d68edc33df3c631571c
github username <hanyc1205@gmail.com>
2020-05-26 12:54:13 -0400

    dropdown and radio button

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -9,12 +9,19 @@ export class ContactFormComponent implements OnInit {
 
   constructor() { }
 
+  contactMethods = [
+    {id:1, name:"Email"},
+    {id:2, name:"Tel"},
+    {id:3, name:"Visit"},
+    {id:4, name:"Telephacy"}
+  ]
+
   log(x){
     console.log(x)
   }
 
   submit(x){
-    console.log(x.value.firstName, x.value.id)
+    console.log(x)
   }
 
   ngOnInit(): void {
