8e6ec0ccfb5fd01693712d481c530d645c9bae48
github username <hanyc1205@gmail.com>
2020-05-29 00:16:50 -0400

    post, delete fixed

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -14,38 +14,42 @@ export class PostComponent implements OnInit {
   constructor(private service:PostService) { 
 
   }
-
-  createPost(input){
-    let title = {title: input.value}
-    input.value= ""
-    this.posts.splice(0,0, title);
-    this.service.createPost(input)
-    .subscribe((res:{id}) => {
-      title['id'] = res.id
-    })
-  }
-
-  onUpdate(item){
-    this.service.createPatch(item)
-      .subscribe(res => {
-        console.log(res); 
-      })
-  }
-
-  onDelete(item){
-    let index = this.posts.indexOf(item)
-    this.posts.splice(index, 1)
-    this.service.onDelete(item)
-      .subscribe(res => {
-
-      })
-  }
-
+  
   ngOnInit(){
     this.service.createGet()
     .subscribe(res => {
       this.posts  = res;
     })
   }
+
+  createPost(input){
+    console.log(input.value)
+    let title = {title: input.value}
+    input.value= ""
+    this.posts.splice(0,0, title);
+    this.service.createPost(title)
+    .subscribe((res:{id}) => {
+      title['id'] = res.id
+    })
+  }
+
+  // onUpdate(item){
+  //   this.service.createPatch(item)
+  //     .subscribe(res => {
+  //       console.log(res); 
+  //     })
+  // }
+
+  onDelete(item){
+    if(confirm("Are you sure??")){
+    this.service.createDelete(item)
+      .subscribe(res => {
+        let index = this.posts.indexOf(item)
+        this.posts.splice(index, 1)
+      })
+    }
+  }
+
+  
 
 }
--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -1,27 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from  '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
   url: string = "https://jsonplaceholder.typicode.com/posts";
-
+  
   constructor(private http : HttpClient) { }
+
+  createGet(): Observable<any[]> {
+    return this.http.get<any>(this.url)
+  }
 
   createPost(title){
     return this.http.post(this.url, JSON.stringify(title))
   }
 
-  createGet(){
-    return this.http.get(this.url)
-  }
+  // createPatch(item){
+  //   return this.http.patch(this.url + "/" + item.id, JSON.stringify({isRead:true}))
+  // }
 
-  createPatch(item){
-    return this.http.patch(this.url + "/" + item.id, JSON.stringify({isRead:true}))
-  }
-
-  onDelete(item){
+  createDelete(item){
     return this.http.delete(this.url+"/"+item.id)
   }
   
