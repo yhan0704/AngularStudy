8463d80431a77e695cef7144be5d1fdd9b569e4e
Young Han <hanyc1205@gmail.com>
2020-05-31 17:44:40 -0400

    binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -9,6 +9,22 @@ export class TestComponent implements OnInit {
 
   constructor() { }
 
+  public name = 'Young Chan';
+  public myId = 'testId';
+  public isdisable = false;
+  public isSpecial = true;
+  public successClass = 'text-success';
+  public hasError = false;
+  public messageClass = {
+    'text-success': !this.hasError,
+    'text-danger': this.hasError,
+    'text-special': this.isSpecial
+  };
+
+  onClick(event){
+    console.log(event);
+  }
+
   ngOnInit(): void {
   }
 
